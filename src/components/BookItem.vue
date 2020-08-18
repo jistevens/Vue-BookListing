9e4c942c7fcb607a858a2b8aaf712780f78e60d5
Jason Stevens <jason.ike.stevens@gmail.com>
2020-08-18 17:59:25 -0500

    Pulled entries out into BookItem component

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template>
+<li>{{ book.title }}: {{ book.author }}</li>
+</template>
+
+<style>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
+
+<script>
+export default {
+  name: 'BookItem',
+  props: ['book'],
+}
+</script>
