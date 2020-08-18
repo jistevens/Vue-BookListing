8f7abec7a2dd1951ac11924ce73c3bd48cfd0e68
Jason Stevens <jason.ike.stevens@gmail.com>
2020-08-18 18:21:10 -0500

    Replaced list elements with BookItems
    It's not actually displaying anything now? Tests pass though.

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,9 +2,7 @@
 <div>
   <h1>{{ title }}</h1>
   <ul>
-    <li v-for="book in books">
-      {{ book.title }}: {{ book.author }}
-    </li>
+    <book-item v-for="book in books" :book="book"></book-item>
   </ul>
 </div>
 </template>
@@ -22,8 +20,11 @@ ul {
 
 
 <script>
+import './BookItem';
+
 export default {
   name: 'BookList',
+  components: { BookItem },
   data() {
     return { 
       title: 'All Books',
